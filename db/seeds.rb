a47e00ec09a957a968cfb3ca090d1b9b20f158aa
Stefanie Lim <stefanie.lim04@gmail.com>
2018-07-27 15:14:13 +0800

    Adds a course model with basic index and show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Course.create!(name: "Defense Against the Dark Arts")
+Course.create!(name: "Intro to Potions")
+Course.create!(name: "Dragons and Hippogriffs")
