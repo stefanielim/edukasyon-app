337c565809ce82149661dd657bb1b0e66bc1f853
Stefanie Lim <stefanie.lim04@gmail.com>
2018-07-31 08:28:37 +0800

    Adds index and show pages for professors in the user facing pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   resources :courses, path: 'classes', only: [:index, :show]
+  resources :professors, only: [:index, :show]
 
   namespace :admin do
     root 'pages#dashboard'
